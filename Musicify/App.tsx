570980a54e1834efc2ba13a57f72ed7b2743954c
Kshitij <guptakshitij2308@gmail.com>
2024-09-07 20:24:38 +0530

    Initialized app for mobile app.

---
+++ Musicify/App.tsx
@@ -0,0 +1,11 @@
+import {Text, SafeAreaView} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello Musicify</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
